492e764e007ad76a458e91dc4a62af800d598c5a
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2025-02-02 15:38:57 +0600

    updated colors abd bg

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,9 +9,9 @@ import './home.css'
 
 const Home = () => {
 
-  useEffect(()=>{
-      window.scroll(0,0)
-    },[])
+  // useEffect(()=>{
+  //     window.scroll(0,0)
+  //   },[])
 
   return (
     <div className="background pb-64 grid grid-cols-1 justify-center items-center">
@@ -20,7 +20,7 @@ const Home = () => {
       <Carousal></Carousal>
 
       <div className="w-[90%] md:w-[80%] mx-auto flex flex-col justify-center items-center">
-        <h3 className="navbar-font text-3xl md:text-5xl text-center mt-20 mb-6 md:pt-14">
+        <h3 className="navbar-font text-3xl md:text-5xl text-center mt-20 mb-6 md:pt-14 bg-[#bdb5b5ac] p-6 rounded ">
           Art&Crafts Items
         </h3>
        
--- src/components/CraftsItems/CraftsItem.jsx
+++ src/components/CraftsItems/CraftsItem.jsx
@@ -3,92 +3,92 @@ import { Link } from "react-router-dom";
 const CraftsItem = () => {
   return (
     <div className=" w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 items-center">
-      <div className="w-full">
-        <div className=" w-full rounded-t-lg bg-[#dedccf] p-4">
+      <div className="w-full shadow-lg">
+        <div className=" w-full rounded-t-lg border-2 border-[#dedccf]  bg-[#dedccfac] p-4">
           <img
             src="https://i.imgur.com/VzSWg9x.jpg"
             alt=""
             className="h-60 w-full rounded-t-md"
           />
         </div>
-        <div className="p-3 text-left bg-gray-200 rounded-b-lg w-full">
+        <div className="p-3 text-left bg-[#c9b99da0] rounded-b-lg w-full">
           <Link to={"/subCategory/LandscapePainting"}>
-            <button className="btn w-full"> Landscape Painting </button>{" "}
+            <button className="btn bg-[#c8c19cf0] border-2 border-[#c9b99d] w-full"> Landscape Painting </button>
           </Link>
         </div>
       </div>
 
-      <div>
-        <div className=" w-full rounded-t-lg bg-[#dedccf] p-4">
+      <div className="w-full shadow-lg">
+        <div className=" w-full rounded-t-lg border-[#dedccf]  bg-[#dedccfac] p-4">
           <img
             src="https://i.imgur.com/OewwaLs.jpg"
             alt=""
             className="h-60 w-full rounded-t-md"
           />
         </div>
-        <div className="p-3 text-left bg-gray-200 rounded-b-lg w-full">
+        <div className="p-3 text-left bg-[#c9b99da0] rounded-b-lg w-full">
           <Link to={"/subCategory/PortraitDrawing"}>
-            <button className="btn w-full"> Portrait Drawing </button>{" "}
+            <button className="btn w-full bg-[#c8c19cf0] border-2 border-[#c9b99d]"> Portrait Drawing </button>{" "}
           </Link>
         </div>
       </div>
 
-      <div>
-        <div className=" w-full rounded-t-lg bg-[#dedccf] p-4">
+      <div className="w-full shadow-lg">
+        <div className=" w-full rounded-t-lg border-[#dedccf]  bg-[#dedccfac] p-4">
           <img
             src="https://i.imgur.com/WtLlLqB.jpg"
             alt=""
             className="h-60 w-full rounded-t-md"
           />
         </div>
-        <div className="p-3 text-left bg-gray-200 rounded-b-lg w-full">
+        <div className="p-3 text-left bg-[#c9b99da0] rounded-b-lg w-full">
           <Link to={"/subCategory/OilPainting"}>
-            <button className="btn w-full"> Oil Painting </button>{" "}
+            <button className="btn w-full bg-[#c8c19cf0] border-2 border-[#c9b99d]"> Oil Painting </button>{" "}
           </Link>
         </div>
       </div>
 
-      <div>
-        <div className=" w-full rounded-t-lg bg-[#dedccf] p-4">
+      <div className="w-full shadow-lg">
+        <div className=" w-full rounded-t-lg border-[#dedccf]  bg-[#dedccfac] p-4">
           <img
             src="https://i.imgur.com/etXYXaM.jpg"
             alt=""
             className="h-60 w-full rounded-t-md"
           />
         </div>
-        <div className="p-3 text-left bg-gray-200 rounded-b-lg w-full">
+        <div className="p-3 text-left bg-[#c9b99da0] rounded-b-lg w-full">
           <Link to={"/subCategory/CharcoalSketching"}>
-            <button className="btn w-full"> Charcoal Sketching </button>{" "}
+            <button className="btn w-full bg-[#c8c19cf0] border-2 border-[#c9b99d]"> Charcoal Sketching </button>{" "}
           </Link>
         </div>
       </div>
 
-      <div>
-        <div className=" w-full rounded-t-lg bg-[#dedccf] p-4">
+      <div className="w-full shadow-lg">
+        <div className=" w-full rounded-t-lg border-[#dedccf]  bg-[#dedccfac] p-4">
           <img
             src="https://i.imgur.com/PHT62pI.jpg"
             alt=""
             className="h-60 w-full rounded-t-md"
           />
         </div>
-        <div className="p-3 text-left bg-gray-200 rounded-b-lg w-full">
+        <div className="p-3 text-left bg-[#c9b99da0] rounded-b-lg w-full">
           <Link to={"/subCategory/WaterColorPainting"}>
-            <button className="btn w-full"> Water Color Painting </button>{" "}
+            <button className="btn w-full bg-[#c8c19cf0] border-2 border-[#c9b99d]"> Water Color Painting </button>{" "}
           </Link>
         </div>
       </div>
 
-      <div>
-        <div className=" w-full rounded-t-lg bg-[#dedccf] p-4">
+      <div className="w-full shadow-lg">
+        <div className=" w-full rounded-t-lg border-[#dedccf]  bg-[#dedccfac] p-4">
           <img
             src="https://i.imgur.com/KlfCSiM.jpg"
             alt=""
             className="h-60 w-full rounded-t-md"
           />
         </div>
-        <div className="p-3 text-left bg-gray-200 rounded-b-lg w-full">
+        <div className="p-3 text-left bg-[#c9b99da0] rounded-b-lg w-full">
           <Link to={"/subCategory/CartoonDrawing"}>
-            <button className="btn w-full"> Cartoon Drawing </button>{" "}
+            <button className="btn w-full bg-[#c8c19cf0] border-2 border-[#c9b99d]"> Cartoon Drawing </button>{" "}
           </Link>
         </div>
       </div>
